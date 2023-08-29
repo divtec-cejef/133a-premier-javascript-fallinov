117e58d3fbf600dd96daefd5397084e60cc1f7b2
Steve Fallet <steve.fallet@gmail.com>
2023-08-29 14:09:58 +0200

    ajout contenu champ dans ul

--- js/main.js
+++ js/main.js
@@ -11,7 +11,20 @@ let h1 = document.querySelector('h1');
 // Retourne tous les <p> dans un tableau
 let tabPara = document.querySelectorAll('p');
 // Affiche le contenu des variables h1 et tabPara
-console.log(h1, tabPara);
+// console.log(h1, tabPara);
 // Afficher le 1er <p>
-console.log(tabPara[0]);
+// console.log(tabPara[0]);
+
+// Récupération bouton Ajouter
+let btAjouter = document.querySelector('.ajouter');
+let inputArticle = document.getElementById('article');
+let ulArticles = document.querySelector('ul.articles');
+// console.log(ulArticles);
+
+// Ecoute événement click du bouton
+btAjouter.addEventListener('click', () => {
+    ulArticles.innerHTML += '<li>' + inputArticle.value + '</li>';
+    // Vide le champ Article
+    inputArticle.value = '';
+});
 
