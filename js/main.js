899dafc304772c9697e7d22c0be4ebf916a2f669
Steve Fallet <steve.fallet@gmail.com>
2023-08-29 12:36:00 +0200

    sélection des paragraphes du document

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Mon premier JavaScript
+ * @author Steve Fallet <steve.fallet@divtec.ch>
+ * @version 0.1
+ * @since 2023.08.29
+ */
+
+'use strict';
+
+let tabPara = document.querySelectorAll('p');
+
+console.log(tabPara);
