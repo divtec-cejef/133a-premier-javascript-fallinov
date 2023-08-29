89bc82fd41d71363fb4a67677e34fa6b2f6eaba1
Fallet Steve <steve.fallet@divtec.ch>
2023-08-29 16:18:42 +0200

    ajout articles dans <ul>

--- js/main.js
+++ js/main.js
@@ -9,6 +9,14 @@
 // Récupère tous les paragraphes du document
 let tabPara = document.querySelectorAll("p");
 let btAjouter = document.querySelector("button.ajouter");
+let txtArticle = document.querySelector("#article");
+let ulArticles = document.querySelector("ul.articles");
 
 // Affiche le contenu des variables dans la console
-console.log(tabPara, btAjouter);
+console.log(tabPara, btAjouter, txtArticle, ulArticles);
+// Ecoute le click sur btAjouter
+btAjouter.addEventListener("click", () => {
+    ulArticles.innerHTML += "<li>" + txtArticle.value + "</li>";
+    // Vide le champ texte
+    txtArticle.value = "";
+});
